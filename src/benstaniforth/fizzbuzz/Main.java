8d96334d343ff0177d8a2b758b1b900775e6d84c
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-02-25 10:57:35 +0000

    First commit

---
+++ src/benstaniforth/fizzbuzz/Main.java
@@ -0,0 +1,20 @@
+package benstaniforth.fizzbuzz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int number = 1; number<101; number++) {
+
+            if (number == 3){
+                System.out.println("Fizz");
+            }
+
+                System.out.println(number);
+
+        }
+
+
+
+    }
+}
