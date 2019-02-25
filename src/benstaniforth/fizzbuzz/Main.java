664b3f364c9f7d5db66a57314536d2fdc4dbe06c
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-02-25 12:01:28 +0000

    Added multiple codes

--- src/benstaniforth/fizzbuzz/Main.java
+++ src/benstaniforth/fizzbuzz/Main.java
@@ -4,17 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int number = 1; number<101; number++) {
+        for (int number = 1; number < 101; number++) {
 
-            if (number == 3){
-                System.out.println("Fizz");
-            }
+            if (number % 3 == 0 && number % 5 == 0){
+                System.out.println("FizzBuzz");}
+            else if (number % 3 == 0) {
+                System.out.println("Fizz");}
+            else if (number % 5 == 0){
+                System.out.println("Buzz");}
+
+
+            else {
 
                 System.out.println(number);
 
+
+            }
         }
 
-
-
+        }
     }
-}
+
+
+
+    // if ((number % 3 == 0) && (number % 5 == 0));{
+    //        System.out.println("FizzBuzz");}
